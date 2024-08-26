c5e647fff9a00416fb7d5c3071add7a8e55db6ad
wanglei <316434776@qq.com>
2024-08-26 21:44:25 +0800

    learn component

--- vite-demo/src/App.vue
+++ vite-demo/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 
+// 引入的组件，如果在  <template>  中使用，首字母必须大写！！！
+import Header from './components/header.vue';
+import Footer from './components/footer.vue'
 </script>
 
 <template>
+
+  <Header />
+
   ctra.top
+
+  <Footer />
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ vite-demo/src/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+   <h3>foot</h3>
+</template>
+
+<script>
+
+</script>
+
+<style  scoped>
+
+</style>

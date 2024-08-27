38931e798d528ea26c5998c069baffdae36756a6
wanglei <316434776@qq.com>
2024-08-27 17:28:49 +0800

    add defineEmits

---
+++ vite-demo-defineEmits/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive, ref } from "vue";
+// 导入子组件
+import Header from "./components/header.vue";
+
+// 响应式数据
+const web = reactive({
+  name: "wangdd",
+  url: "ctra.top"
+})
+
+const user = ref(0)
+
+// 子传父
+const emitsWeb = (data) => {
+  console.log("emitsWeb:", data);
+  web.url = data.url
+}
+
+const emitsUser = (data) => {
+  console.log("emitsUser:", data);
+  user.value += data
+}
+
+</script>
+
+<template>
+  <!-- 子传父 -->
+  <Header @web="emitsWeb" @user="emitsUser" />
+  {{ web.name }} :{{ web.url }} -{{ user }}
+</template>
+
+<style scoped></style>
---
+++ vite-demo-defineEmits/src/components/header.vue
@@ -0,0 +1,24 @@
+<script setup>
+// 子组件
+/**
+ * defineEmits 是 vue3 的编译时宏函数，
+ * 用于子组件向父组件发送自定义事件
+ */
+// 定义一个名为 emits的对象，用于存储自定义事件
+const emits = defineEmits(["web", "user"])
+// 发送名为 web 和 user的自定义事件
+emits("web", { name: "wanglei", url: "ctra.top" })
+// 添加用户
+const userAdd = () => {
+    // 发送名为user的自定义事件
+    emits("user", 10)
+}
+</script>
+
+<template>
+    <h3>Header</h3>
+
+    <button @click="userAdd">添加用户</button>
+</template>
+
+<style scoped></style>
